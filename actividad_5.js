2d6ee2e8f5ecaa5b7675b155068cd8da12f2a031
Melissa G <a334259@uach.mx>
2021-03-19 21:52:08 -0700

    Se agregaron dos archivos de la actividad 5, uno .js y uno .html

---
+++ actividad_5.js
@@ -0,0 +1,6 @@
+        //function mostrarfecha() {
+    	//	document.getElementById('fecha').innerHTML = Date();
+    	//}
+        const mostrarfecha = () => {
+    		document.getElementById('fecha').innerHTML = Date();
+    	}
